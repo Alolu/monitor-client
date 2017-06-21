228c9e62c729973721adffa7210b2e36be875ba6
Alolu-pc <borniche.leo@gmail.com>
2017-06-21 15:56:24 +0200

    fix notif+edit

---
+++ src/components/Alert.vue
@@ -0,0 +1,20 @@
+  <template>
+	  <div id="alert" class="alert alert-dismissible fade show" :class="[type]" data-dismiss="alert" role="alert">
+	    {{ msg }}
+	  </div>
+  </template>
+  <script>
+  	export default {
+  		props: ['type','msg'],
+      created: function(){
+        this.deleteAlert();
+      },
+      methods: {
+        deleteAlert(){
+          setTimeout(function(){
+            $('#alert').alert('close');
+          },2000);
+        }
+      }
+	}
+  </script>
